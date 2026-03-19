1e8a60a71a754ca5ec21aae5d63a20f63fcd0517
Akuzwe Achille <akuachille2006@gmail.com>
2026-03-19 14:54:35 +0200

    initialization of GitIgnore file

--- Web/obj/Debug/net10.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net10.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be005072a8b08f16583ee90426072ae98d646543")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+930db77d9e9208c443754d78ee85759ab4c328e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
